0f9d33437aa0ec18a2808fe73c487afc44fd6f87
Mdbshero <mdbshero@gmail.com>
2022-02-18 20:18:30 -0600

    added variables

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -1,2 +1,15 @@
 "use strict";
+//Day Trip Generator
+//As a user, I want a destination to be randomly selected for my day trip. 5
+//As a user, I want a restaurant to be randomly selected for my day trip. 5
+//As a user, I want a mode of transportation to be randomly selected for my day trip. 5
+//As a user, I want a form of entertainment to be randomly selected for my day trip. 5
+//As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things. 15
+//As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections. 10
+//As a user, I want to display my completed trip in the console/alert. 10
+//As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing! 10
 
+let destinations = ['Cool Springs', 'East Nashville', 'Broadway', 'Spring Hill', 'Green Hills'];
+let restaurants = ['Chauhan', 'Emmy Squared', 'International Market', 'Tansuo', 'House of Cards'];
+let transportation = ['Automobile', 'Bus', 'Uber', 'Lyft'];
+let entertainment = ['Honkey Tonks', 'Magic Show', 'Grand ol Opry', 'Art Museum'];
